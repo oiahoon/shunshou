9f835c368aa00b2e7abd215fa536125d1a87dfb3
Joey@macstudio <4296411@qq.com>
2026-09-08 22:14:20 +0800

    Retain Radix MIT notices in generated icon assets

--- ops/build-home-icons.mjs
+++ ops/build-home-icons.mjs
@@ -1,9 +1,10 @@
 import { createRequire } from 'node:module';
-import { mkdirSync, writeFileSync } from 'node:fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
 const require = createRequire(new URL('../prototypes/shunshou/package.json', import.meta.url));
 const React = require('react');
 const { renderToStaticMarkup } = require('react-dom/server');
 const icons = require('@radix-ui/react-icons');
+const license = readFileSync(new URL('../prototypes/shunshou/node_modules/@radix-ui/react-icons/LICENSE', import.meta.url), 'utf8');
 const selected = {
   'arrow-up-right': 'ArrowTopRightIcon', 'arrow-down': 'ArrowDownIcon',
   'arrow-right': 'ArrowRightIcon', 'arrow-left': 'ArrowLeftIcon',
@@ -15,6 +16,6 @@ const symbols = Object.entries(selected).map(([id, name]) =>
   `<symbol id="${id}" viewBox="0 0 24 24">${renderToStaticMarkup(React.createElement(icons[name], { width: 24, height: 24 }))}</symbol>`).join('');
 const dir = new URL('../services/resolver/public/assets/', import.meta.url);
 mkdirSync(dir, { recursive: true });
-writeFileSync(new URL('icons.svg', dir), `<svg xmlns="http://www.w3.org/2000/svg">${symbols}</svg>`);
+writeFileSync(new URL('icons.svg', dir), `<!-- ${license} -->\n<svg xmlns="http://www.w3.org/2000/svg">${symbols}</svg>`);
 const arrow = renderToStaticMarkup(React.createElement(icons.ArrowTopRightIcon, { width: 44, height: 44, color: '#ffcc29' }));
-writeFileSync(new URL('favicon.svg', dir), `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect width="64" height="64" rx="8" fill="#101010"/><g transform="translate(10 10)">${arrow}</g></svg>`);
+writeFileSync(new URL('favicon.svg', dir), `<!-- ${license} -->\n<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect width="64" height="64" rx="8" fill="#101010"/><g transform="translate(10 10)">${arrow}</g></svg>`);
